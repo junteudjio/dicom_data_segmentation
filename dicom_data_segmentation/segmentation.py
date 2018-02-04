5925dcffe69294bdb19b07d3c1deb63c2833e134
teudjio <teudjiombativou@gmail.com>
2018-02-04 06:45:10 +0100

    add fix treshold hist segmentation

--- dicom_data_segmentation/segmentation.py
+++ dicom_data_segmentation/segmentation.py
@@ -1,1 +1,13 @@
+import numpy as np
+
+from abstract_segmentation import AbstractSegmentation
+
 __author__ = 'Junior Teudjio'
+
+
+class FixedTresholdHistogramSegmentation(AbstractSegmentation):
+    def do_segmentation(self, sample, threshold=0.21):
+        img_norm, o_contour = sample['img-norm'], sample['o-contour']
+        threshold_mask = img_norm > threshold
+        pred_i_contour = np.logical_and(o_contour, threshold_mask)
+        sample['pred-i-contour'] = pred_i_contour
