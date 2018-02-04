899c817efb8135d4b606d5f84fa00bfeacc72ed2
teudjio <teudjiombativou@gmail.com>
2018-02-04 07:19:20 +0100

    add run_data_parser script; add parsed _data and _plots

---
+++ dicom_data_segmentation/run__data_parser.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import dicom_data_pipeline.run__data_parser
+__author__ = 'Junior Teudjio'
+'''Running this script will forward the execution of the parsing to the dicom_data_pipeline package.'''
